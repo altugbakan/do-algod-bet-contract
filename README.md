ebf6a86863b782b244848693586fd302b7b9a579
altugbakan <altugbakan@gmail.com>
2022-03-14 23:21:37 +0300

    feat: added contract addresses

---
+++ README.md
@@ -0,0 +1,3 @@
+# Do Kwon vs Tetranode LUNA Bet Contract
+
+More info [here](https://twitter.com/stablekwon/status/1503264558160375810).
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -8,8 +8,8 @@ from scripts.helpful_scripts import get_account
 def deploy(account: Account = get_account()):
     return Bet.deploy(
         config["networks"][network.show_active()]["price_feed"],
-        config["networks"][network.show_active()]["bet_token"],
-        1_000_000 * 10 ** 18,  # 1 million dollars
+        config["networks"][network.show_active()]["bet_token"],  # USDT
+        1_000_000 * 10 ** 18,  # 1 million USDT
         datetime.strptime("13/3/2023", "%d/%m/%Y").timestamp(),  # March 13th, 2023
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify", False),
